62b950dd3141757b1f764f3ba4895c0182260735
John Carr <john.carr@isotoma.com>
2011-09-15 18:44:11 +0100

    Add todo extension

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['yaydoc', 'sphinx.ext.intersphinx']
+extensions = ['yaydoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
