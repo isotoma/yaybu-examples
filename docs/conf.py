7b1b5466df4fb4c872ea8a29036eab19c0fb4bb5
John Carr <john.carr@isotoma.com>
2011-09-15 18:40:35 +0100

    Main document is index.rst

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'site'
+master_doc = 'index'
 
 # General information about the project.
 project = u'Yaybu Example Configuration'
